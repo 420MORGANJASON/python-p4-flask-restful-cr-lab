10b659c66ccba3c4c62252649d85abbde676d42f
MORGANJASON <420morganjason@gmail.com>
2023-07-09 21:33:20 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -7,3 +7,10 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    
+    
+    def __repr__(self):
+        return f'<Plants {self.name} is this price {self.price}.>'
